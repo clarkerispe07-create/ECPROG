7c508ca9f96ec92d1b835f53d562cf7c858ebec8
Clark Dee <263336227+clarkerispe07-create@users.noreply.github.com>
2026-06-19 13:21:07 +0800

    Add files via upload

--- file.c
+++ file.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
-    int main() {
+float getCelsius(){
+    float c;
+    
+    printf("Enter the number of Celsius: ");
+    scanf("%f", &c);
+    return c;
+}
 
-        printf("Hello World");
+float convertToFahrenheit(float celsius){
+    float result = (celsius*9/5)+32;
+    return result;
+}
 
-        return 0;
-    }
+void displayResult(float celsius, float fahrenheit){
+    printf("%.2f Celsius is %.2f Fahrenheit\n", celsius, fahrenheit);
+}
+
+int main(){
+    
+        float a = getCelsius();
+          float b = convertToFahrenheit(a);
+          displayResult(a,b);
+    
+    return 0;
+}
